389ceafd9074c4b2767c4948e945b50d1fe90ac3
ramkishorsharma-afk <ramkishor.sharma@gmail.com>
2026-03-30 20:46:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const Razorpay = require('razorpay');
 const { getKundali } = require('./astro');
-const { interpretKarmicSymptoms } = require('./interpreter');
+const { interpretKarmicSymptoms } = require('./Interpreter');
 
 const app = express();
 app.use(cors());
